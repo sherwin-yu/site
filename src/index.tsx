acfbeaeede58739f50b26940b58a3cef2fe224ba
Sherwin Yu <sherwinhyu@gmail.com>
2025-09-23 12:09:29 -0400

    typo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
